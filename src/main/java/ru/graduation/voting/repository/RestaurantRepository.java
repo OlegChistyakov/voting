375b8d82de750841b30cb9e03ae58075e70e0afb
”OlegCh” <heleg.chist@gmail.com>
2021-07-21 13:39:46 +0300

    add repository

---
+++ src/main/java/ru/graduation/voting/repository/RestaurantRepository.java
@@ -0,0 +1,29 @@
+package ru.graduation.voting.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.graduation.voting.model.Dish;
+import ru.graduation.voting.model.Restaurant;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
+
+@Repository
+@Transactional(readOnly = true)
+public class RestaurantRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    Restaurant getWithMenuByDate(Integer id, LocalDate date) {
+        Restaurant restaurant = em.createQuery("SELECT r FROM Restaurant r WHERE r.id=:id", Restaurant.class)
+                .setParameter("id", id)
+                .getSingleResult();
+        restaurant.setMenu(em.createQuery("SELECT d FROM Dish d WHERE d.restaurant.id = :rest_id AND d.date=:date", Dish.class)
+                .setParameter("rest_id", restaurant.getId())
+                .setParameter("date", date)
+                .getResultList());
+        return restaurant;
+    }
+}
