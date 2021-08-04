9f90f5aee36c31e75e0a23307c8cf3a146485d48
OlegCh <heleg.chist@gmail.com>
2021-08-04 13:55:04 +0300

    add security util

---
+++ src/main/java/ru/graduation/voting/controller/SecurityUtil.java
@@ -0,0 +1,20 @@
+package ru.graduation.voting.controller;
+
+import ru.graduation.voting.model.AbstractEntity;
+
+public class SecurityUtil {
+
+    private static int id = AbstractEntity.START_SEQ;
+
+    private SecurityUtil() {
+    }
+
+    public static int authUserId() {
+        return id;
+    }
+
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
+    }
+
+}
