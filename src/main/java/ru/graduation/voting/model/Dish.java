e6b981cc89552dcf817e6bea7d6221631abab4f5
OlegCh <heleg.chist@gmail.com>
2021-08-05 13:42:23 +0300

    customize object mapper

--- src/main/java/ru/graduation/voting/model/Dish.java
+++ src/main/java/ru/graduation/voting/model/Dish.java
@@ -1,6 +1,6 @@
 package ru.graduation.voting.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,7 +26,7 @@ public class Dish extends AbstractEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "rest_id")
-    @JsonIgnore
+    @JsonBackReference
     private Restaurant restaurant;
 
     public Dish(Integer id, String description, int price, LocalDate date, Restaurant restaurant) {
